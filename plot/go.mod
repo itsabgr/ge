48ca00f17bb99e6cd7ccd8e078792394c74509d6
abgr <tbpmtbpm@gmail.com>
2024-10-07 11:10:45 +0330

    impl plot tree

---
+++ plot/go.mod
@@ -0,0 +1,10 @@
+module github.com/itsabgr/ge/plot
+
+go 1.23
+
+require (
+	github.com/itsabgr/ge v0.0.0
+	github.com/xlab/treeprint v1.2.0
+)
+
+replace github.com/itsabgr/ge v0.0.0 => ..
---
+++ plot/tree.go
@@ -0,0 +1,58 @@
+package plot
+
+import (
+	"fmt"
+	"github.com/itsabgr/ge"
+	_ "github.com/itsabgr/ge"
+	"github.com/xlab/treeprint"
+	_ "github.com/xlab/treeprint"
+)
+
+type treeNode struct {
+	Tree    treeprint.Tree
+	Count   int
+	IsMulti bool
+}
+
+func Tree(root error) interface {
+	Bytes() []byte
+	fmt.Stringer
+} {
+
+	if root == nil {
+		return nil
+	}
+
+	treeMap := map[int]*treeNode{}
+
+	for depth, err := range ge.Walk(root) {
+		node := &treeNode{}
+		switch err.(type) {
+		case ge.UnwrapError:
+			if depth == 0 {
+				node.Tree = treeprint.NewWithRoot(err)
+			} else {
+				node.Tree = treeMap[depth-1].Tree.AddBranch(err)
+			}
+		case ge.UnwrapErrors:
+			node.IsMulti = true
+			if depth == 0 {
+				node.Tree = treeprint.NewWithRoot(0)
+			} else {
+				node.Tree = treeMap[depth-1].Tree.AddBranch(0)
+			}
+		default:
+			if depth == 0 {
+				node.Tree = treeprint.NewWithRoot(err)
+			} else {
+				node.Tree = treeMap[depth-1].Tree.AddNode(err)
+			}
+		}
+		treeMap[depth] = node
+		if prev := treeMap[depth-1]; prev != nil && prev.IsMulti {
+			prev.Count += 1
+			treeMap[depth-1].Tree.SetValue(prev.Count)
+		}
+	}
+	return treeMap[0].Tree
+}
